e5af6bccd230768adf879f77aa0b30f1c06bd8c4
Kinder112 <pawel.beder@gmail.com>
2016-08-20 10:32:56 +0200

    HealthCheck object

---
+++ src/main/java/hello/HealthCheck.java
@@ -0,0 +1,23 @@
+package hello;/*
+    Created by kinder112 on 20.08.2016.
+ */
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class HealthCheck {
+    private String currentTime;
+
+    public String getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(String currentTime) {
+        this.currentTime = currentTime;
+    }
+
+    @Override
+    public String toString() {
+        return currentTime;
+    }
+}
