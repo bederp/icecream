5093be95b47438335697d1e25d71afa409fc5021
stolawoj <stolawoj@WL312105.eu.tieto.com>
2016-08-20 11:30:17 +0200

    greeting stub

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ *
+ * @author stolawoj
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Greeting {
+    private String greeting;
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting{" + "greeting=" + greeting + '}';
+    }
+    
+}
