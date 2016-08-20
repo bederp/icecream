8328c735fc2ec8425a0eaad9d4a1b9a9c05e6618
stolawoj <stolawoj@WL312105.eu.tieto.com>
2016-08-20 15:05:52 +0200

    VisitedPoints

--- src/main/java/bot/Botv2.java
+++ src/main/java/bot/Botv2.java
@@ -14,11 +14,13 @@ import org.slf4j.LoggerFactory;
 import java.util.*;
 
 public class Botv2 {
+
     private Point startPosition;
     private Point currentPosition;
     private Point endPosition;
     private Deque<Methods> moves = new ArrayDeque<>();
-    private ClientApi client = new Client("r1_1", false);
+    private Map<Point, Boolean> visitedPoints = new HashMap<>();
+    private ClientApi client = new Client("r1_2", false);
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
@@ -27,30 +29,52 @@ public class Botv2 {
         log.info("Starting Competition");
         startCompetition();
         while (!currentPosition.equals(endPosition)) {
-            move1Step();
+            log.info("Current moves stack: {}", moves);
+            Methods move = pickMoveDirection();
+            move1Step(move);
         }
         log.info("Finished maze");
     }
 
-    private void initialize() {
-        moves.addFirst(Methods.MoveUp);
-        moves.addFirst(Methods.MoveLeft);
-        moves.addFirst(Methods.MoveDown);
-        moves.addFirst(Methods.MoveRight);
+    private Methods pickMoveDirection() {
+        moves.clear();
+        prioritizeMoves(Methods.MoveRight, Methods.MoveDown, Methods.MoveLeft, Methods.MoveUp);
+        return moves.pop();
     }
 
-    private void move1Step() {
-            final Methods move = moves.pop();
-            final MoveResult moveResult = client.move(move);
-            if(moveResult.isSucces()){
-                log.info("Moved:" + move);
-                moves.addFirst(move);
-                reorderOppositeMoveToEndOfStack(move);
-                currentPosition = moveResult.getPosition();
-            }else {
-                moves.add(move);
-                log.info("Blocked: " + move);
-            }
+    private void prioritizeMoves(Methods... moveDirs) {
+        for (Methods method : moveDirs) {
+            prioritizeMove(method);
+        }
+    }
+
+    private void prioritizeMove(Methods moveDir) {
+        final Point dir = getNewPosition(moveDir);
+        if (visitedPoints.get(dir) != null) {
+            moves.addLast(moveDir);
+        }
+    }
+
+    private void initialize() {
+        moves.addLast(Methods.MoveRight);
+        moves.addLast(Methods.MoveDown);
+        moves.addLast(Methods.MoveLeft);
+        moves.addLast(Methods.MoveUp);
+    }
+
+    private void move1Step(Methods move) {
+        final MoveResult moveResult = client.move(move);
+        currentPosition = moveResult.getPosition();
+        visitedPoints.put(currentPosition, true);
+
+        if (moveResult.isSucces()) {
+            log.info("Moved:" + move);
+            moves.addFirst(move);
+            reorderOppositeMoveToEndOfStack(move);
+        } else {
+            moves.addLast(move);
+            log.info("Blocked: " + move);
+        }
     }
 
     private void reorderOppositeMoveToEndOfStack(Methods move) {
@@ -59,15 +83,40 @@ public class Botv2 {
         moves.addLast(oppositeMove);
     }
 
-
     private void startCompetition() {
         final StartCompetition startCompetition = client.startCompetition();
         log.info(String.valueOf(startCompetition));
         currentPosition = startPosition = startCompetition.getStartPoint();
+        visitedPoints.put(startPosition, Boolean.TRUE);
         endPosition = startCompetition.getEndPoint();
     }
 
+    private boolean wasThereBefore(Methods move) {
+        final Point newPosition = getNewPosition(move);
+        if (visitedPoints.get(newPosition) != null) {
+            log.info("Change of move. Was before at {}", newPosition);
+            return true;
+        }
+        return false;
+    }
 
-
+    private Point getNewPosition(Methods move) {
+        Point newPosition = new Point(currentPosition);
+        switch (move) {
+            case MoveDown:
+                newPosition.moveDown();
+                break;
+            case MoveUp:
+                newPosition.moveUp();
+                break;
+            case MoveLeft:
+                newPosition.moveLeft();
+                break;
+            case MoveRight:
+                newPosition.moveRight();
+                break;
+        }
+        return newPosition;
+    }
 
 }
--- src/main/java/json/Point.java
+++ src/main/java/json/Point.java
@@ -5,11 +5,23 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 /*
     Created by kinder112 on 20.08.2016.
  */
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Point {
+
     private int x;
     private int y;
+
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Point p) {
+        this(p.getX(), p.getY());
+    }
 
     public void setX(int x) {
         this.x = x;
@@ -19,6 +31,30 @@ public class Point {
         this.y = y;
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void moveUp() {
+        this.y -= 1;
+    }
+
+    public void moveDown() {
+        this.y += 1;
+    }
+
+    public void moveLeft() {
+        this.x -= 1;
+    }
+
+    public void moveRight() {
+        this.x += 1;
+    }
+
     @Override
     public String toString() {
         return x + " " + y;
@@ -26,12 +62,18 @@ public class Point {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Point point = (Point) o;
 
-        if (x != point.x) return false;
+        if (x != point.x) {
+            return false;
+        }
         return y == point.y;
 
     }
